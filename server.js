7cb42c7dc0bf0a2f4c15d34fd603b637049e5f30
Thiago Behm <thiagobehm@yahoo.com.br>
2018-12-27 23:23:17 -0200

    implemented login validation

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.use(paginate.middleware(10, 50));
 
 // initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
-    key: 'user_session',
-    secret: 'migrateUsersRock',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
+  key: 'user_session',
+  secret: 'migrateUsersRock',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
 }));
 
 
@@ -52,14 +52,14 @@ app.post('/migrate', urlencodedParser, (req, resp) => {
     },
     json: true
   }
-
+  
   request(options)
-    .then((data)=>{
-      resp.send(data);
-    })
-    .catch((err)=>{
-      resp.send(err);
-    });
+  .then((data)=>{
+    resp.send(data);
+  })
+  .catch((err)=>{
+    resp.send(err);
+  });
 });
 
 app.get('/', urlencodedParser, (req, resp) => {
@@ -69,26 +69,30 @@ app.get('/', urlencodedParser, (req, resp) => {
 app.post('/authenticate', urlencodedParser, (req, resp) => {
   let username = req.body.username;
   let password = req.body.password;
-  let instance = req.body.instance;
-
+  
   let authentication =  base64.encode(`${username}:${password}`);
-
-
-  request.get( {
+  
+  if(!req.body.instance) {
+    resp.render('index.hbs', {
+      error: "Please provide the instance URL!!!",
+    });//end of the render
+  } else {
+    instance = req.body.instance;
+    request.get( {
       url: `https://${instance}.atlassian.net/rest/api/2/issue/createmeta`,
-    json: true,
-  }, (err, res, body) => {
-    // if result it's ok    
-    if (res.statusCode === 200) {
-      
-      req.session.authentication = authentication;
-      req.session.instance = instance;
-      resp.redirect('/getUsers');
-    } else { //401 = invalid credentials
-        console.log(res.statusCode);
-    }
-  })//end or get request
+      json: true,
+    }, (err, res, body) => {
+      // if result it's ok    
+      if (res.statusCode === 200) {
+        
+        req.session.authentication = authentication;
+        req.session.instance = instance;
+        resp.redirect('/getUsers');
+      } 
+    });//end or get request
+  }
 });
+
 
 //url just to render the page in case the customer access /authentication
 app.get('/authentication', urlencodedParser, (req, resp) => {
@@ -97,19 +101,19 @@ app.get('/authentication', urlencodedParser, (req, resp) => {
 
 //link to the page that renders the request
 app.get('/convert', urlencodedParser, (req, resp) =>{
-
+  
   request.get( {
     url: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user?active-filter=active&start-index=1&max-results=1000`,
-      headers: {
-        'authorization': `Basic ${req.session.authentication}`        
-      },
-      json: true,
-    }, (err, res, body) => {
+    headers: {
+      'authorization': `Basic ${req.session.authentication}`        
+    },
+    json: true,
+  }, (err, res, body) => {
     // if result it's ok    
     if (res.statusCode === 200) {
       resp.render('convert.hbs');
     } else { //401 = invalid credentials
-        resp.redirect("/authentication");
+      resp.redirect("/authentication");
     }
   })//end or get request
   
@@ -117,25 +121,27 @@ app.get('/convert', urlencodedParser, (req, resp) =>{
 
 
 app.get('/getUsers', urlencodedParser, (req, resp, next) => {
-
+  
   request.get( {
-      url: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user?active-filter=active&start-index=0&max-results=1000`,
-      headers: {
-        'authorization': `Basic ${req.session.authentication}`        
-      },
-      json: true,
+    url: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user?active-filter=active&start-index=0&max-results=1000`,
+    headers: {
+      'authorization': `Basic ${req.session.authentication}`        
+    },
+    json: true,
   }, (err, res, body) => {
     // if result it's ok    
     if (res.statusCode === 200) {
       //const pageCount = Math.ceil(30 / req.query.limit);
-
+      
       resp.render('getUsers.hbs', {
         users: body.localCustomers,
         //pageCount,
         //pages: paginate.getArrayPages(req)(3, pageCount, req.query.page)
       });
     } else { //401 = invalid credentials        
-        resp.redirect('/authentication');
+      resp.render('index.hbs', {
+        error: "Invalid credentials! Please, confirm your user and password and as well make sure that you have access to this instance...",
+      });//end of the render
     }
   });//end or get request
 });
