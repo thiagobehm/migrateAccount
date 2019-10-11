bec03feeef706a6ef22b7e1864bc17618a3456e2
Thiago Luis Vasconcellos Behm <thiagoluisbehm@gmail.com>
2019-10-11 13:16:22 -0300

    added CSV option

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const path = require('path');
 const base64 = require('base-64');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const paginate = require('express-paginate');
+const multer = require('multer');
+const fs = require('fs');
+const csv = require('csv-parse')
+
 
 //configures a variable to heroku environment
 const port = process.env.PORT || 3000;
 
 //starts express
 let app = express();
-
 
 //parses the body parameters
 let urlencodedParser = bodyParser.urlencoded({
@@ -27,7 +29,18 @@ app.set('view engine', 'hbs');
 //defines the 'root' directory for public files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(paginate.middleware(10, 50));
+
+const upload = multer({
+  'preservePath': true,
+  //storage: multer.memoryStorage(),
+  dest: 'tmp/',
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.csv$/)) {
+      return cb('Please upload a CSV file!')
+    }
+    cb(undefined, true)
+  }
+});
 
 // initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
@@ -44,34 +57,63 @@ app.get('/', urlencodedParser, (req, resp) => {
   resp.render('index.hbs');
 });
 
-app.post('/authenticate', urlencodedParser, (req, resp) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let action = req.body.action;
+async function read_csv(csv_file) {
+  let idColumnFound = false;
+  let emailColumnFound = false;
+  let readStream = fs.createReadStream(csv_file.path);
+  let header = true;
+  let idLocation = 0;
+  let emailLocation = 0;
+  let result = [ ];
+  let obj;
 
-  let authentication =  base64.encode(`${username}:${password}`);
+  readStream
+  .pipe(csv())
+  .on('data', (data) => {
+      //use an object instead
+      // result.push(data);
+        if (header) {
+        for (let i = 0; i < data.length; i++) {          
+          if (data[i] == 'username') {
+            idLocation = i;
+            idColumnFound = true;
+            break;
+          }
+        }
+
+        for (let i = 0; i < data.length; i++) {          
+          if (data[i] == 'email') {
+            emailLocation = i;
+            emailColumnFound = true;
+            break;
+          }
+        }
+        header = false;   
+      } else {
+        obj = {
+          username: data[idLocation],
+          emailAddress: data[emailLocation]
+        }
+        result.push(obj);              
+      }    
+    })
+  .on('error', function (err) {
+    console.log(err);
+  })
+  .on('end', function () {
+    fs.unlink(csv_file.path, function (err) {
+      if (err) {
+        console.log(err)
+      }
+    });
+  })
   
-  if(!req.body.instance) {
-    resp.render('index.hbs', {
-      error: "Please provide the instance URL!!!",
-    });//end of the render
-  } else {
-    instance = req.body.instance;
-    request.get( {
-      url: `https://${instance}.atlassian.net/rest/api/2/issue/createmeta`,
-      json: true,
-    }, (err, res, body) => {
-      // if result it's ok    
-      if (res.statusCode === 200) {
-        
-        req.session.authentication = authentication;
-        req.session.instance = instance;
-        req.session.action = action;
-        resp.redirect('/getUsers');
-      } 
-    });//end or get request
-  }
-});
+  // if (!idColumnFound && !emailColumnFound) {
+  //   throw new Error('Please make sure that your CSV file has at least a column named "username" and "email"');
+  // }
+
+  return result;
+}
 
 app.get('/logout', (req, resp)=>{
   req.session.destroy(()=>{
@@ -95,32 +137,59 @@ app.get('/convert', urlencodedParser, (req, resp) =>{
   }  
 });
 
-app.get('/getUsers', urlencodedParser, (req, resp, next) => {
+app.post('/getUsers', upload.single('csv_file'), (req, resp, next) => {
 
-  let filter = req.session.action === 'activate' ? 'inactive' : 'active';
-  
-  request.get( {
-    url: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user?active-filter=${filter}&start-index=0&max-results=5000`,
-    headers: {
-      'authorization': `Basic ${req.session.authentication}`        
-    },
-    json: true,
-  }, (err, res, body) => {
+  let username = req.body['username'];
+  let password = req.body['password'];
+  let action = req.body['action'];
+  let instance;
+  let csv_file = req.file;
+  let authentication =  base64.encode(`${username}:${password}`);
+  let filter = action === 'activate' ? 'inactive' : 'active';
+
+  if(!req.body['instance']) {
+    resp.render('index.hbs', {
+      error: "Please provide the instance URL!!!",
+    });//end of the render
+  } else {
+    instance = req.body['instance'];
+
+    request.get({
+      // url: `https://${instance}.atlassian.net/rest/api/2/issue/createmeta`,
+      url: `https://${instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user?active-filter=${filter}&start-index=0&max-results=500`,
+      headers: {
+        'authorization': `Basic ${authentication}`        
+      },
+      json: true,
+    }, (err, res, body) => {
     // if result it's ok    
     if (res.statusCode === 200) {
-      //const pageCount = Math.ceil(30 / req.query.limit);
-  
-      resp.render('getUsers.hbs', {
-        users: body.localCustomers,
-        //pageCount,
-        //pages: paginate.getArrayPages(req)(3, pageCount, req.query.page)
-      });
+      req.session.authentication = authentication;
+      req.session.instance = instance;
+      req.session.action = action;
+
+      if (csv_file) {
+        read_csv(csv_file).then((data)=>{          
+          resp.render('getUsers.hbs', {
+            users: data,            
+          });
+        }).catch((err)=>{
+          resp.render('index.hbs', {
+            error: err.message
+          })
+        })
+      } else {
+        resp.render('getUsers.hbs', {
+          users: body.localCustomers,        
+        });
+      }      
     } else { //401 = invalid credentials        
       resp.render('index.hbs', {
         error: "Invalid credentials! Please, confirm your user and password and as well make sure that you have access to this instance...",
       });//end of the render
     }
   });//end or get request
+  }//else
 });
 
 app.post('/migrate', urlencodedParser, (req, resp) => {
@@ -151,7 +220,7 @@ app.post('/deactivate', urlencodedParser, (req, resp) => {
   let user = req.body.username;
 
   let options = {
-  
+
     uri: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user/deactivate/?username=${user}`,
     // uri: `https://admin.atlassian.com/gateway/api/adminhub/customer-directory/directory/9d1a867a-c37b-4f88-961b-b716cdbfaee1/user/${user}`,
     method: 'PUT',
@@ -174,7 +243,7 @@ app.post('/activate', urlencodedParser, (req, resp) => {
   let user = req.body.username;
 
   let options = {
-  
+
     uri: `https://${req.session.instance}.atlassian.net/rest/servicedesk/customer-management/noeyeball/1/local-servicedesk-user/activate/?username=${user}`,
     // uri: `https://admin.atlassian.com/gateway/api/adminhub/customer-directory/directory/9d1a867a-c37b-4f88-961b-b716cdbfaee1/user/${user}`,
     method: 'PUT',
